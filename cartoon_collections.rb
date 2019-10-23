b563e5983aed85606917cbcfd4b2310303a4705d
Michael Gerland <mr.gerland@gmail.com>
2019-10-23 21:01:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,7 @@ def find_the_cheese(array)
 
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  array.find {|x| if array.include?(x) return x}
+  array.find {|x| if array.include?(x)
+    return x}
   return nil
 end
