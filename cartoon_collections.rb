c550561dae096e31490f93d68b4c3790aa846f8e
Michael Gerland <mr.gerland@gmail.com>
2019-10-23 20:40:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?(array)
+  array.include?(array) {|x| x}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
